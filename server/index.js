2c58d568e7a2bbbb5496829099402e8c540893b4
LorenzoBozzoni <32290911+LorenzoBozzoni@users.noreply.github.com>
2022-10-05 17:00:45 +0200

    aggiunta connessione mongo e login base

--- server/index.js
+++ server/index.js
@@ -5,13 +5,25 @@ const cors = require("cors");
 const {Server} = require("socket.io");
 const mysql = require('mysql');
 const Partita = require("./partita.js");
+const {MongoClient} = require("mongodb");
+const { stringify } = require("querystring");
+
 
 app.use(cors());
+
+const dbURI = "mongodb+srv://lorebozzo:-4J2Rht4QYq6S!!@cluster0.3wfvfel.mongodb.net/?retryWrites=true&w=majority"
+const mongoClient = new MongoClient(dbURI);
+
+async function run() {
+  await mongoClient.connect();
+  console.log("Siamo connessi a MongoDB Atlas!");
+}
+run().catch(err => console.log("Errore connessione: " + err))
 
 const server = http.createServer(app);
 const io = new Server(server, {
     cors : {
-        origin : "http://localhost:3000",
+        origin : ["http://localhost:3000","http://localhost:3001"],
         methods : ["GET", "HEAD", "OPTIONS", "POST", "PUT", "DELETE"]    // methods accepted
     }
 })
@@ -21,9 +33,38 @@ var multi = [];
 var friend = [];
 var partite = [];
 
+const db = mongoClient.db("Briscola");
+const collection = db.collection('Briscola');
+
+
+
+
 io.on("connection", (socket) =>{
     console.log(socket.id);
+    socket.on("access", async (accessMode, email, password) => {      // async qua per aspettare risultato query mongodb
+      console.log("accessMode:",accessMode)
+      console.log("email:",email)
+      console.log("password:",password)
+      if (accessMode === "signup"){
+        const insertResult = collection.insertOne({user: email, password: password});      // tolto async --> AGGIUNGERE HASH!
+        if (insertResult)
+          socket.emit("accessOutcome", true);
+        else 
+          socket.emit("accessOutcome", false);
 
+        //console.log('Inserted documents =>', insertResult);
+      }else{
+        // login
+        const selectResult = await collection.findOne({user: email, password: password});
+        console.log("selectResult: ", selectResult)
+        if (selectResult)
+          socket.emit("accessOutcome", true);
+        else 
+          socket.emit("accessOutcome", false);   
+      }
+    })
+
+    /*
     socket.on("startGame", (mode) => {
 
       switch (mode) {
@@ -44,14 +85,11 @@ io.on("connection", (socket) =>{
 
       
     })
-
-
+    */
 
     socket.on("disconnect", () =>{
       console.log("user disconnected ", socket.id);
     })
-
-
 })
 
 
