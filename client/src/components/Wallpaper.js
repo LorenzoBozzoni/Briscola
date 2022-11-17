b3d93a9a6acd9c82d7562e0d9f1fc1244cdb4f08
Lele <maggiraffaele@gmail.com>
2022-11-17 16:45:25 +0100

    modificato lo sfondo

--- client/src/components/Wallpaper.js
+++ client/src/components/Wallpaper.js
@@ -1,40 +1,37 @@
 import React, {Component} from 'react'
 import "./Wallpaper.css"
 
-
-
-
 export class Wallpaper extends Component {
   render (){
     return(
     <>
 <section>
           <div className="card">
-            <div className='back'></div>
-            <div className='back'></div>
-            <div className='back'></div>
-            <div className='back'></div>
-            <div className='back'></div>
-            <div className='back'></div>
-            <div className='back'></div>
+            <div className='back rounded-4'></div>
+            <div className='back rounded-4'></div>
+            <div className='back rounded-4'></div>
+            <div className='back rounded-4'></div>
+            <div className='back rounded-4'></div>
+            <div className='back rounded-4'></div>
+            <div className='back rounded-4'></div>
           </div>
           <div className="card card1">
-            <div className='back'></div>
-            <div className='back'></div>
-            <div className='back'></div>
-            <div className='back'></div>
-            <div className='back'></div>
-            <div className='back'></div>
-            <div className='back'></div>
+            <div className='back rounded-4'></div>
+            <div className='back rounded-4'></div>
+            <div className='back rounded-4'></div>
+            <div className='back rounded-4'></div>
+            <div className='back rounded-4'></div>
+            <div className='back rounded-4'></div>
+            <div className='back rounded-4'></div>
           </div>
           <div className="card card2">
-            <div className='back'></div>
-            <div className='back'></div>
-            <div className='back'></div>
-            <div className='back'></div>
-            <div className='back'></div>
-            <div className='back'></div>
-            <div className='back'></div>
+            <div className='back rounded-4'></div>
+            <div className='back rounded-4'></div>
+            <div className='back rounded-4'></div>
+            <div className='back rounded-4'></div>
+            <div className='back rounded-4'></div>
+            <div className='back rounded-4'></div>
+            <div className='back rounded-4'></div>
           </div>
         </section>
         </>
